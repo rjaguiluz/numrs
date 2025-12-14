d454ab3e186bd6c6ef8a5b83ffb5d02eed143715
Rodrigo <rj.aguiluz@gmail.com>
2025-12-14 00:59:05 -0300

    chore: bump version to v0.1.12 and fix linux path export

--- bindings/numrs-py/setup.py
+++ bindings/numrs-py/setup.py
@@ -8,7 +8,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name="numrs",
-    version="0.1.11",
+    version="0.1.12",
     description="Python bindings for NumRs",
     packages=find_packages(),
     install_requires=[
