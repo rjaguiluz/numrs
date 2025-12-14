ec8e2d3e73a42e834eba7363ab06d85c83419028
Rodrigo <rj.aguiluz@gmail.com>
2025-12-13 23:41:50 -0300

    chore: bump version to v0.1.7 and add .npmignore for wasm

--- bindings/numrs-py/setup.py
+++ bindings/numrs-py/setup.py
@@ -8,7 +8,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name="numrs",
-    version="0.1.6",
+    version="0.1.7",
     description="Python bindings for NumRs",
     packages=find_packages(),
     install_requires=[
