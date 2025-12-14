8b9118a504f0cf7b495c9ee66dba7375eca36600
Rodrigo <rj.aguiluz@gmail.com>
2025-12-14 10:37:35 -0300

    chore: bump version to v0.1.15 and add musllinux CI support

--- bindings/numrs-py/setup.py
+++ bindings/numrs-py/setup.py
@@ -8,7 +8,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name="numrs",
-    version="0.1.14",
+    version="0.1.15",
     description="Python bindings for NumRs",
     packages=find_packages(),
     install_requires=[
