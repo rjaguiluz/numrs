ac8586866eb11a6a235331b0eab3514547de173a
Rodrigo <rj.aguiluz@gmail.com>
2025-12-14 00:49:18 -0300

    chore: bump version to v0.1.11 and robustify linux CI cmd

--- bindings/numrs-py/setup.py
+++ bindings/numrs-py/setup.py
@@ -8,7 +8,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name="numrs",
-    version="0.1.10",
+    version="0.1.11",
     description="Python bindings for NumRs",
     packages=find_packages(),
     install_requires=[
