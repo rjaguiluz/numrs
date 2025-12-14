aba70fa9daf4626302eb64138fff45b8cecc9e55
Rodrigo <rj.aguiluz@gmail.com>
2025-12-14 10:52:45 -0300

    chore: bump version to v0.1.16 and install libnumrs_c to system path

--- bindings/numrs-py/setup.py
+++ bindings/numrs-py/setup.py
@@ -8,7 +8,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name="numrs",
-    version="0.1.15",
+    version="0.1.16",
     description="Python bindings for NumRs",
     packages=find_packages(),
     install_requires=[
