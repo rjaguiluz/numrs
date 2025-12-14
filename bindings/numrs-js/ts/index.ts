dbdef135ce1c568788f46eebc41d5f00ab9faa3f
Rodrigo <rj.aguiluz@gmail.com>
2025-12-13 22:49:18 -0300

    feat(exports): expose TrainerBuilder, Dataset, nn, and NativeTensor in index.ts

--- bindings/numrs-js/ts/index.ts
+++ bindings/numrs-js/ts/index.ts
@@ -3,4 +3,4 @@ export * as autograd from './autograd';
 export { Tensor } from './autograd';
 export * as ops from './ops';
 export * as data from './data';
-export { version } from './native_loader';
+export { version, TrainerBuilder, Dataset, NativeTensor, nn } from './native_loader';
