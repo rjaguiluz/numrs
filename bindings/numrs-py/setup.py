d5cf96c0256c36cc56a6e11bba3b896ee5aafbfe
Rodrigo <rj.aguiluz@gmail.com>
2025-12-13 22:41:44 -0300

    ci: parallelize wasm and node builds in release workflow

--- bindings/numrs-py/setup.py
+++ bindings/numrs-py/setup.py
@@ -8,7 +8,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name="numrs",
-    version="0.1.4",
+    version="0.1.3",
     description="Python bindings for NumRs",
     packages=find_packages(),
     install_requires=[
